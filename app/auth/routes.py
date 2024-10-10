afff7115eb3591343ca736687bd56d6b9fe890da
Skw1kr <matveev.skywalker@gmail.com>
2024-10-10 20:04:30 +0400

    52

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -6,7 +6,7 @@ auth_bp = Blueprint('auth', __name__)
 # подключение к базе данных
 def get_db_connection():
     conn = sqlite3.connect('fckdtbs.db')
-    conn.row_factory = sqlite3.Row
+    conn.row_factory = sqlite3.Row  # для удобного обращения к колонкам по именам
     return conn
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
@@ -15,36 +15,36 @@ def login():
         login = request.form['login']
         password = request.form['password']
 
-        # подключение к базе данных и проверка пользователя
         conn = get_db_connection()
         user = conn.execute('SELECT * FROM users WHERE login = ? AND password = ?', (login, password)).fetchone()
         conn.close()
 
         if user:
-            session['user_id'] = user['login']  # Сохранение логина пользователя в сессии
-            return redirect(url_for('auth.profile'))  # Переадресация на страницу профиля
+            session['user_id'] = user['login']  
+            return redirect(url_for('auth.profile'))  
         else:
-            flash('Неправильный логин или пароль')  # Сообщение об ошибке
+            flash('Неправильный логин или пароль')  
             return redirect(url_for('auth.login'))
 
     return render_template('login.html')
 
 @auth_bp.route('/profile')
 def profile():
-    # проверяем, есть ли пользователь в сессии
     if 'user_id' not in session:
-        return redirect(url_for('auth.login'))  # Переадресация на страницу логина, если пользователь не авторизован
+        return redirect(url_for('auth.login'))  
     
-    # отображаем страницу профиля
-    return render_template('profile.html', user=session['user_id'])
+    user_id = session['user_id']  
+  
+    conn = get_db_connection()
+    
+    user_data = conn.execute("SELECT login, balance FROM users WHERE login = ?", (user_id,)).fetchone()
+    conn.close()
 
-    # проверяем, вошел ли пользователь
-    if 'username' not in session:
-        return redirect(url_for('auth.login'))
-    
-    # показываем профиль пользователя
-    username = session['username']
-    return render_template('profile.html', username=username)
+    if user_data:
+        username, balance = user_data['login'], user_data['balance']  
+        return render_template('profile.html', username=username, balance=balance)
+    else:
+        return "User not found", 404
 
 
 @auth_bp.route('/signup', methods=['GET', 'POST'])
@@ -55,7 +55,7 @@ def signup():
 
         # подключение к базе данных и добавление пользователя
         conn = get_db_connection()
-        conn.execute('INSERT INTO users (login, password) VALUES (?, ?)', (username, password))
+        conn.execute('INSERT INTO users (login, password, balance) VALUES (?, ?, ?)', (username, password, 1000.0))
         conn.commit()
         conn.close()
 
@@ -66,5 +66,5 @@ def signup():
 @auth_bp.route('/logout')
 def logout():
     # удаляем пользователя из сессии
-    session.pop('username', None)
+    session.pop('user_id', None)  # удаляем user_id из сессии
     return redirect(url_for('auth.login'))
